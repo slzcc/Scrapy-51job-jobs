8563ddad42bd6cf06fbef6f3e240c2ac2a15d6c3
石磊 <383821949@qq.com>
2017-04-24 16:01:19 +0800

    first commit

--- company_resume_51job/company_name.py
+++ company_resume_51job/company_name.py
@@ -11,6 +11,8 @@ import threading
 REDIS_HOST = os.getenv('REDIS_DB_HOST')
 REDIS_PORT = int(os.getenv('REDIS_DB_PORT'))
 
+threads = []
+
 redis_q = redis.StrictRedis(connection_pool=redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0))
 
 class TaskThread(threading.Thread):
@@ -19,16 +21,21 @@ class TaskThread(threading.Thread):
         self.Data = data
 
     def run(self):
+        global redis_q
         locks.acquire()
+        url = self.Data.xpath('p/span/a/@href')[0]
+        redis_q.lpush('51job:start_urls', url)
         locks.release()
 
 def PositionUrl(Html):
     selector = html.fromstring(Html)
     position = selector.xpath('//div[@id="resultList"]/div[@class="el"]')
-    locks = threading.Lock()
     for i in position:
-        url = i.xpath('p/span/a/@href')[0]
-        redis_q.lpush('51job:start_urls', url)
+        T = TaskThread(i)
+        T.start()
+        threads.append(T)
+    for t in threads:
+        t.join()
 
 
 print("Please according to the prompt to input, must strictly careful operation in order to avoid waste your how many repair operations.")
@@ -39,16 +46,16 @@ Pages = 1
 urls = 'http://search.51job.com/list/000000,000000,0000,00,9,99,{0},2,{1}.html'.format(Company_name, Pages)
 
 Request = requests.get(url=urls).content
-PositionUrl(Request)
 
 selector = html.fromstring(Request)
 
 NumberPages = selector.xpath('//div[@class="dw_table"]/div[@class="dw_tlc"]/div[@class="rt"]/text()')[2].split('/ ')[1]
 
-for i in range(2, int(NumberPages)+1):
+for i in range(0, int(NumberPages)+1):
     urls = 'http://search.51job.com/list/000000,000000,0000,00,9,99,{0},2,{1}.html'.format(Company_name, i)
     print(urls)
     Request = requests.get(url=urls).content
+    locks = threading.Lock()
     PositionUrl(Request)
 
 print("执行完毕请查看 Redis 队列信息.")
